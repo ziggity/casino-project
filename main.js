3d43d0cbf28c8376160a47e68eb41e389061051f
zachbeecher <zachbeecher@gmail.com>
2023-11-16 11:06:42 -0800

    added images to show up after you press shuffle, draw 2 cards, draw cards for dealer (although we need to hide one of them, and draw one for each player and dealer (we need to hide the dealer cards next step)

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const backImageOfCard = "https://www.deckofcardsapi.com/static/img/back.png";
 const NEW_DECK_API_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle/?count=1';
+const cardQuerySelector = document.querySelector(".card");
+const cardDealerQuerySelector = document.querySelector(".cardDealer");
+const cardsLeftInDeckQuerySelector = document.querySelector(".cardsLeftInDeck");
+
 let deckID;
 let cardsRemainingInDeck;
 let dealerScore;
 let playerScore;
+
 
 async function createCardDeckAndGetID () {
     const res = await fetch(NEW_DECK_API_URL);
@@ -17,21 +22,55 @@ async function createCardDeckAndGetID () {
 async function drawCards() {
     const res = await fetch('https://www.deckofcardsapi.com/api/deck/' + deckID + '/draw/?count=2');
     const data = await res.json();
-    cardsRemainingInDeck = data.remaining;
+    let newCard = "";
     for(let card of data.cards) {
     console.log("drawn card test ", card.image, card.value, card.suit);
-    
+    newCard += `<img class="card" src="${card.image}" alt="${card.value + " of " + card.suit}"'/>`;
     }
+    cardQuerySelector.innerHTML = newCard;
 }
-
-async function drawOneCard() {
+async function drawCardsDealer(){
+    const res = await fetch('https://www.deckofcardsapi.com/api/deck/' + deckID + '/draw/?count=2');
+    const data = await res.json();
+    let newCardDealer = "";
+    cardsRemainingInDeck = data.remaining;
+    for(let card of data.cards) {
+    console.log("drawn card test dealer: ", card.image, card.value, card.suit);
+    newCardDealer += `<img class="cardDealer" src="${card.image}" alt="${card.value + " of " + card.suit}"'/>`;
+    }
+    cardDealerQuerySelector.innerHTML = newCardDealer;
+};
+async function drawOneCardDealer() {
     const res = await fetch('https://www.deckofcardsapi.com/api/deck/' + deckID + '/draw/?count=1');
     const data = await res.json();
     cardsRemainingInDeck = data.remaining;
-    for(let i = 0 ; i <= 52; i++) {
-   // console.log("drawn card test ", cardsRemainingInDeck.cards[i]);
-    }
+    newCardDealer = "";
+    for(let card of data.cards) {
+        console.log("drawn card test dealer: ", card.image, card.value, card.suit);
+        newCardDealer += `<img class="cardDealer" src="${card.image}" alt="${card.value + " of " + card.suit}"'/>`;
+        }
+        cardQuerySelector.innerHTML = newCardDealer;
 }
+
+async function drawOneCardPlayer() {
+    const res = await fetch('https://www.deckofcardsapi.com/api/deck/' + deckID + '/draw/?count=1');
+    const data = await res.json();
+    cardsRemainingInDeck = data.remaining;
+    newCardPlayer = "";
+    for(let card of data.cards) {
+        console.log("drawn card test dealer: ", card.image, card.value, card.suit);
+        newCardPlayer += `<img class="cardDealer" src="${card.image}" alt="${card.value + " of " + card.suit}"'/>`;
+        }
+        cardQuerySelector.innerHTML = newCardPlayer;
+}
+// this function isn't legal - so we can skip it
+
+// async function checkCardsLeftInDeck() {
+//     const res = await fetch('https://www.deckofcardsapi.com/api/deck/' + deckID + '/draw/?count=2');
+//     const data = await res.json();
+//     cardsLeftInDeckQuerySelector =  `"There are "+ ${data.remaining} + " cards left in deck."`;
+// }
+
 function openSettingtoStart(){
     document.querySelector(".navbar-setting").classList.toggle("start");
     document.querySelector(".setting").classList.toggle("menu");
