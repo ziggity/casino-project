629be09587e356467288f7d6d9ca60ad86547eb7
Nahal Abie <nahal.abie@gmail.com>
2023-11-16 15:56:44 +0100

    creating first page and adding some event listener

--- main.js
+++ main.js
@@ -30,7 +30,15 @@ async function drawOneCard() {
     console.log("drawn card test ", data.cards[i]);
     }
 }
+function openSettingtoStart(){
+    document.querySelector(".navbar-setting").classList.toggle("start");
+    document.querySelector(".setting").classList.toggle("menu");
+}
+function backToFirstPage(){
+    document.querySelector(".navbar-setting").classList.toggle("start");
+    document.querySelector(".setting").classList.toggle("menu");
 
+}
 
 
 // dealerPlays(){
