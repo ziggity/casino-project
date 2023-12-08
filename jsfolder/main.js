bd0064558740e90a3ab0464790fb22912d3029a0
David Ross <officialdavidross@gmail.com>
2023-12-08 03:33:35 -0800

    Simplified Start page until properly built

---
+++ jsfolder/main.js
@@ -0,0 +1,10 @@
+function openSettingtoStart() {
+    document.querySelector(".navbar-setting").classList.toggle("start");
+    document.querySelector(".setting").classList.toggle("menu");
+  }
+  function backToFirstPage() {
+    document.querySelector(".navbar-setting").classList.toggle("start");
+    document.querySelector(".setting").classList.toggle("menu");
+  }
+  
+  
